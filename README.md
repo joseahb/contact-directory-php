373d4a63b28494e1e688b6d1ffec05b623fd3870
joseahb <biwottjoseah@gmail.com>
2021-11-04 12:39:52 +0300

    Add instruction

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Contact directory php
+System uses flat-file system to store data
+
+# Setup
+
+- Copy to server(e.g htdocs for xampp)
+
+- create directory file named  `directory.php` in `includes` folder
+
+- Verify permissions for file
--- includes/process.php
+++ includes/process.php
@@ -62,22 +62,70 @@
         header("Location: ../info.php");
     }
     if (isset($_GET['q'])) {
-        if (!$direditor->check_dir_file()) {
+        // Validate
+        // Validate
+        if (empty($_GET['fname'])) {
+            $_SESSION['message'] = "Contact first name is required for search";
+            header("Location: ../info.php");
+            exit;
+        }
+        if (empty($_GET['lname'])) {
+            $_SESSION['message'] = "Contact last name is required for search";
+            header("Location: ../info.php");
+            exit;
+        }
+        $found = $direditor->find_contact($_GET['fname'], $_GET['lname']);
+        if ($found == false) {
+            $_SESSION['message'] = "Contact name ".$_GET['fname'] ." ". $_GET['lname']." not found";
             header("Location: ../info.php");
         }
         else {
-           $found = $direditor->find_contact($_GET['fname'], $_GET['lname']);
-           if ($found == false) {
-                $_SESSION['message'] = "Contact name ".$_GET['fname'] ." ". $_GET['lname']." not found";
-                header("Location: ../info.php");
-           }
-           else {
-                $_SESSION['single'] = $found;
-                header("Location: ../result.php");
-           }
+            $_SESSION['single'] = $found;
+            header("Location: ../result.php");
         }
     }
     if (isset($_POST['update_contact'])) {
+        // Validate
+        if (empty($_POST['firstname'])) {
+            $_SESSION['message'] = "Contact first name is required";
+            header("Location: ../info.php");
+            exit;
+        }
+        if (empty($_POST['lastname'])) {
+            $_SESSION['message'] = "Contact last name is required";
+            header("Location: ../info.php");
+            exit;
+        }
+        if (empty($_POST['email'])) {
+            $_SESSION['message'] = "Contact email is required";
+            header("Location: ../info.php");
+            exit;
+        }
+        if (empty($_POST['phone'])) {
+            $_SESSION['message'] = "Contact phone number is required";
+            header("Location: ../info.php");
+            exit;
+        }
+        if (empty($_POST['street'])) {
+            $_SESSION['message'] = "Contact Street address is required";
+            header("Location: ../info.php");
+            exit;
+        }
+        if (empty($_POST['state'])) {
+            $_SESSION['message'] = "Contact state is required";
+            header("Location: ../info.php");
+            exit;
+        }
+        if (empty($_POST['city'])) {
+            $_SESSION['message'] = "Contact city is required";
+            header("Location: ../info.php");
+            exit;
+        }
+        if (empty($_POST['zip'])) {
+            $_SESSION['message'] = "Contact zip is required";
+            header("Location: ../info.php");
+            exit;
+        }
         $contact = array(
             'oldfname' => $_POST['oldfname'],
             'oldlname' => $_POST['oldlname'],
